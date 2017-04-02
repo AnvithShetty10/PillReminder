f2ddc465c2a170a96f947f4578ada4f5aeeec24a
Anas Khan <anas.khan96@outlook.com>
2017-04-03 01:05:04 +0530

    updating the ios file

--- application/PillReminder/index.ios.js
+++ application/PillReminder/index.ios.js
@@ -1,53 +1,83 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	View,
+	Button,
+	Text,
 } from 'react-native';
+import { SegmentedControls } from 'react-native-radio-buttons';
 
-export default class PillReminder extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+class PillReminder extends Component {
+	clickHandler() {
+		fetch('http://anask.xyz/ledger/react-check', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				led: this.state.selectedLed,
+				time: this.state.selectedTime,
+			})
+		}).then((response) => response.json())
+			.then((responseJson) => {
+				this.setState({
+					showText: JSON.stringify(responseJson)
+				})
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+
+	constructor() {
+		super();
+		this.state = { showText: null, selectedTime: 'None', selectedLed: 'None' };
+
+		this.clickHandler = this.clickHandler.bind(this);
+	}
+	render() {
+		let timeOptions = [
+    		"9 AM",
+    		"3 PM",
+			"6 PM"
+  		];
+		let LedOptions = [
+			"LED 1",
+			"LED 2",
+			"LED 3",
+			"LED 4",
+			"LED 5",
+			"LED 6"
+		];
+
+  		function setSelectedLed(selectedLed){
+			this.setState({selectedLed});
+  		}
+		function setSelectedTime(selectedTime){
+			this.setState({selectedTime});
+		}
+		let check = this.state.showText ? this.state.showText : "Nothing";
+		return (
+			<View style={{ padding: 20, flex: 1 }}>
+				<SegmentedControls
+ 	 				options={ LedOptions }
+  					onSelection={ setSelectedLed.bind(this) }
+  					selectedOption={ this.state.selectedLed }
+					//direction="column" --- commenting this till the issue is fixed on GitHub
+				/>
+				<SegmentedControls
+ 	 				options={ timeOptions }
+  					onSelection={ setSelectedTime.bind(this) }
+  					selectedOption={ this.state.selectedTime }
+				/>
+				<Text>Selected LED: {this.state.selectedLed}</Text>
+				<Text>Select Time: {this.state.selectedTime}</Text>
+				<Button onPress={this.clickHandler} title="Proceed" color="#841584"/>
+				<Text>{check}</Text>
+			</View>
+		);
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('PillReminder', () => PillReminder);
