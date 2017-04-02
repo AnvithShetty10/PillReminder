6e7e231ef68f71115a2d35a3c172568340aef819
Anas Khan <anas.khan96@outlook.com>
2017-04-03 00:51:37 +0530

    everything is now segmented controls

--- application/PillReminder/index.android.js
+++ application/PillReminder/index.android.js
@@ -32,65 +32,49 @@ class PillReminder extends Component {
 
 	constructor() {
 		super();
-		this.state = { showText: null };
+		this.state = { showText: null, selectedTime: 'None', selectedLed: 'None' };
 
 		this.clickHandler = this.clickHandler.bind(this);
 	}
 	render() {
-		const options = [
+		const timeOptions = [
     		"9 AM",
     		"3 PM",
 			"6 PM"
   		];
+		const LedOptions = [
+			"LED 1",
+			"LED 2",
+			"LED 3",
+			"LED 4",
+			"LED 5",
+			"LED 6",
+		];
 
-  		function setSelectedOption(selectedOption){
-    		this.setState({selectedOption});
+  		function setSelectedLed(selectedLed){
+			this.setState({selectedLed});
   		}
-		let check = this.state.showText ? this.state.showText : "Click on any one button (with time) to proceed";
+		function setSelectedTime(selectedTime){
+			this.setState({selectedTime});
+		}
+		let check = this.state.showText ? this.state.showText : "Nothing";
 		return (
 			<View style={{ padding: 20, flex: 1 }}>
-				<Button
-          style={{ padding: 20 }}
-					onPress={this.clickHandler}
-					title="LED 1 ON"
-					color="#841584"
-				/>
-				<Button
-          style={{ padding: 20 }}
-					onPress={this.clickHandler}
-					title="LED 2 ON"
-					color="#841584"
-				/>
-				<Button
-          style={{ padding: 20 }}
-					onPress={this.clickHandler}
-					title="LED 3 ON"
-					color="#841584"
-				/>
-				<Button
-          style={{ padding: 20 }}
-					onPress={this.clickHandler}
-					title="LED 4 ON"
-					color="#841584"
-				/>
-				<Button
-          style={{ padding:20 }}                    
-					onPress={this.clickHandler}
-					title="LED 5 ON"
-					color="#841584"
-				/>
-				<Button
-          style={{ padding:20 }}
-					onPress={this.clickHandler}
-					title="LED 6 ON"
-					color="#841584"
+				<SegmentedControls
+ 	 				options={ LedOptions }
+  					onSelection={ setSelectedLed.bind(this) }
+  					selectedOption={ this.state.selectedLed }
+					//direction="column" --- commenting this till the issue is fixed on GitHub
 				/>
 				<SegmentedControls
- 	 				options={ options }
-  					onSelection={ setSelectedOption.bind(this) }
-  					selectedOption={ this.state.selectedOption }
+ 	 				options={ timeOptions }
+  					onSelection={ setSelectedTime.bind(this) }
+  					selectedOption={ this.state.selectedTime }
 				/>
-				<Text style={{ padding: 20 }}>{check}</Text>
+				<Text>Selected LED: {this.state.selectedLed}</Text>
+				<Text>Select Time: {this.state.selectedTime}</Text>
+				<Button onPress={this.clickHandler} title="Proceed" color="#841584"/>
+				<Text>{check}</Text>
 			</View>
 		);
 	}
